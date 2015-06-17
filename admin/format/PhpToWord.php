d9d01b2494e07c5758fa82053db66f690afb4e58
Nova Herdi Kusumah <novaherdi@gmail.com>
2015-06-17 08:10:29 +0700

    test please!!

--- admin/format/PhpToWord.php
+++ admin/format/PhpToWord.php
@@ -38,16 +38,67 @@ class PhpToWord
 
     public static function getData($kode_kegiatan)
     {
-        $row = array();
+        $row = array(
+            'data_lelang' => array(),
+            'rka_skpd'    => array(),
+            'pejabat_kpa' => array(),
+            'penyedia'    => array(),
+        );
         $result = mysql_query('SELECT * FROM `data_lelang` WHERE `kode_kegiatan` = "' . $kode_kegiatan . '"');
-        $row['data_lelang'] = mysql_fetch_assoc($result);
+        if ($result) {
+            $row['data_lelang'] = mysql_fetch_assoc($result);
+        }
 
         $result = mysql_query('SELECT * FROM `rka_skpd` WHERE `kode_kegiatan` = "' . $kode_kegiatan . '"');
-        $row['rka_skpd'] = mysql_fetch_assoc($result);
+        if ($result) {
+            $row['rka_skpd'] = mysql_fetch_assoc($result);
+        }
 
-//        $result = mysql_query('SELECT * FROM `penyedia` WHERE `kode_kegiatan` = "' . $kode_kegiatan . '"');
-//        $row['penyedia'] = mysql_fetch_assoc($result);
+        $result = mysql_query('SELECT * FROM `pejabat_kpa` WHERE `kode_kegiatan` = "' . $kode_kegiatan . '"');
+        if ($result) {
+            $row['pejabat_kpa'] = mysql_fetch_assoc($result);
+        }
+
+        $result = mysql_query('SELECT * FROM `penyedia` WHERE `id_penyedia` = "' . $row['data_lelang']['id_penyedia'] . '"'
+        );
+        if ($result) {
+            $row['penyedia'] = mysql_fetch_assoc($result);
+        }
 
         return $row;
+    }
+
+    public static function dateFormatIndo($date)
+    {
+        if($date == '0000-00-00') {
+            return '';
+        } else {
+            $BulanIndo = array(
+                "Januari",
+                "Februari",
+                "Maret",
+                "April",
+                "Mei",
+                "Juni",
+                "Juli",
+                "Agustus",
+                "September",
+                "Oktober",
+                "November",
+                "Desember"
+            );
+
+            $tahun = substr($date, 0, 4);
+            $bulan = substr($date, 5, 2);
+            $tgl   = substr($date, 8, 2);
+
+            $result = $tgl . " " . $BulanIndo[(int) $bulan - 1] . " " . $tahun;
+            return ($result);
+        }
+    }
+
+    public static function numberFormatIndo($number)
+    {
+        return number_format($number, 0, ',', '.');
     }
 }
--- admin/spk.php
+++ admin/spk.php
@@ -12,13 +12,35 @@ if(isset($kode_kegiatan) || !empty($kode_kegiatan)) {
 //    die(json_encode($data));
 
     $templateProcessor->setValue('tahun', htmlspecialchars(date('Y')));
-    $templateProcessor->setValue('tanggal', htmlspecialchars(date('d M Y')));
+    $templateProcessor->setValue('tgl_kontrak', htmlspecialchars(PhpToWord::dateFormatIndo($data['data_lelang']['tgl_kontrak'])));
+    $templateProcessor->setValue('hari_kontrak', htmlspecialchars($data['data_lelang']['hari_kontrak']));
+    $templateProcessor->setValue('bln_kontrak', htmlspecialchars($data['data_lelang']['bln_kontrak']));
     $templateProcessor->setValue('nama_kegiatan', htmlspecialchars($data['data_lelang']['nama_kegiatan']));
     $templateProcessor->setValue('panjang', htmlspecialchars($data['rka_skpd']['panjang']));
     $templateProcessor->setValue('lebar', htmlspecialchars($data['rka_skpd']['lebar']));
     $templateProcessor->setValue('lok_kegiatan', htmlspecialchars($data['rka_skpd']['lok_kegiatan']));
-
-
+    $templateProcessor->setValue('masa_pemel', htmlspecialchars($data['data_lelang']['masa_pemel']));
+    $templateProcessor->setValue('terbilang_pemel', htmlspecialchars($data['data_lelang']['terbilang_pemel']));
+    $templateProcessor->setValue('harga_nego', htmlspecialchars(PhpToWord::numberFormatIndo($data['data_lelang']['harga_nego'])));
+    $templateProcessor->setValue('harga_nego_95', htmlspecialchars(PhpToWord::numberFormatIndo($data['data_lelang']['harga_nego'] * 0.95)));
+    $templateProcessor->setValue('harga_nego_5', htmlspecialchars(PhpToWord::numberFormatIndo($data['data_lelang']['harga_nego'] * 0.05)));
+    $templateProcessor->setValue('terbilang_nego', htmlspecialchars($data['data_lelang']['terbilang_nego']));
+    $templateProcessor->setValue('masa_pelaksanaan', htmlspecialchars($data['data_lelang']['masa_pelaksanaan']));
+    $templateProcessor->setValue('terbilang_masa_pelaks', htmlspecialchars($data['data_lelang']['terbilang_masa_pelaks']));
+    $templateProcessor->setValue('waktu_pelaksanaan', htmlspecialchars($data['data_lelang']['waktu_pelaksanaan']));
+    $templateProcessor->setValue('nama_prsh', htmlspecialchars($data['penyedia']['nama_prsh']));
+    $templateProcessor->setValue('alamat_prsh', htmlspecialchars($data['penyedia']['alamat']));
+    $templateProcessor->setValue('nama_direktur', htmlspecialchars($data['penyedia']['nama_direktur']));
+    $templateProcessor->setValue('tgl_akte', htmlspecialchars(PhpToWord::dateFormatIndo($data['penyedia']['tgl_akte'])));
+    $templateProcessor->setValue('no_akte', htmlspecialchars($data['penyedia']['no_akte']));
+    $templateProcessor->setValue('notaris', htmlspecialchars($data['penyedia']['notaris']));
+    $templateProcessor->setValue('norek', htmlspecialchars($data['penyedia']['norek']));
+    $templateProcessor->setValue('bank', htmlspecialchars($data['penyedia']['bank']));
+    $templateProcessor->setValue('hari_splspmk', htmlspecialchars($data['data_lelang']['hari_splspmk']));
+    $templateProcessor->setValue('bln_splspmk', htmlspecialchars($data['data_lelang']['bln_splspmk']));
+    $templateProcessor->setValue('lingkup_kerja', htmlspecialchars($data['penyedia']['lingkup_kerja']));
+    $templateProcessor->setValue('pejabat_kpa', htmlspecialchars(strtoupper($data['pejabat_kpa']['pejabat_kpa'])));
+    $templateProcessor->setValue('nip', htmlspecialchars($data['pejabat_kpa']['nip']));
 
     PhpToWord::downloadFileTemplate($templateProcessor, 'spk');
 }
